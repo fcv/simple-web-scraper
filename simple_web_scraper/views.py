151b0840653ee74098f3f50f4a5f0cf3f39ca1f9
Fabio Veronez <fabio.veronez@gmail.com>
2016-10-06 10:34:48 -0300

    Add first test case using Json Path

--- simple_web_scraper/views.py
+++ simple_web_scraper/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 
 def articles(request):
-    return HttpResponse("[{'id': 1, 'title': 'Fake Article'}]")
+    return HttpResponse('[{"id": 1, "title": "Fake Article"}]')
--- test/test_http_request.py
+++ test/test_http_request.py
@@ -1,5 +1,7 @@
 import unittest
 from django.test import Client
+import json
+from jsonpath_rw import jsonpath, parse
 
 
 class HttpRequestTest(unittest.TestCase):
@@ -9,3 +11,10 @@ class HttpRequestTest(unittest.TestCase):
         client = Client()
         response = client.get('/api/rest/v1/articles')
         self.assertEqual(response.status_code, 200)
+        content = str(response.content, encoding='utf8')
+        content_json = json.loads(content)
+
+        # see more about jsonpath at http://goessner.net/articles/JsonPath/
+        # and its python port at https://github.com/kennknowles/python-jsonpath-rw
+        actual_ids = [match.value for match in parse('$.[*].id').find(content_json)]
+        self.assertIn(1, actual_ids)

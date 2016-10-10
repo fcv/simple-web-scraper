fad59b6266118f3799bf70b1057b7f265143692c
Fabio Veronez <fabio.veronez@gmail.com>
2016-10-09 23:05:59 -0300

    Set settings' DEBUG to False by default

--- simple_web_scraper/settings.py
+++ simple_web_scraper/settings.py
@@ -24,7 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')yl85h8y*n5hy8w&_j*sba&%bj9$ad0xmy@&d%q038xu)j90gy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG's default values has been set to False and should be changed to True
+# in local's untracked file `local_settings.py`
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
